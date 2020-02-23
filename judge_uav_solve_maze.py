1fc7b2519923e092b06c2ade901c41989077e054
smart-swarm <smartswarm@qq.com>
2020-02-23 16:00:40 +0800

    add hard maze map.

--- judge_uav_solve_maze.py
+++ judge_uav_solve_maze.py
@@ -2,17 +2,26 @@
 import rospy
 import math
 from nav_msgs.msg import Odometry
-target_x = 12
-target_y = 9
+import time
+target_x = 19
+target_y = 19
+start_x = -20
+start_y = 19
+start_time = 0
 
 def pose_CB(data):
     # print(data)
     x = data.pose.pose.position.x
     y = data.pose.pose.position.y
-    dist = math.sqrt((x-target_x)*(x-target_x)+(y-target_y)*(y-target_y))
+    dist_end = math.sqrt((x-target_x)*(x-target_x)+(y-target_y)*(y-target_y))
+    dist_start = math.sqrt((x - start_x) * (x - start_x) + (y - start_y) * (y - start_y))
     z = data.pose.pose.position.z
-    if (z < 5.0 and dist < 1.0):
+    if (z > 1.0 && dist_start < 1.0):
+        start_time = time.time()
+    if (z < 5.0 and dist_end < 1.0):
         rospy.loginfo(rospy.get_caller_id() + " Congratulations! Solved the maze successfully.")
+        if (start_time > 0):
+            rospy.loginfo(rospy.get_caller_id() + " Time Cost: " + time.time() - start_time)
 
 def judge():
     # In ROS, nodes are uniquely named. If two nodes with the same
