b69381d9368ce20ae4ad1d871e66dc3b2b2f59f4
smart-swarm <smartswarm@qq.com>
2020-03-29 23:55:19 +0800

    support multiple mecanum cars.

--- test_car1_close_loop_control.py
+++ test_car1_close_loop_control.py
@@ -3,6 +3,7 @@
 import roslib
 import rospy
 import math
+import time
 
 from geometry_msgs.msg import Twist  	 # for sending commands to the drone
 from std_msgs.msg import Empty       	 # for land/takeoff/emergency
@@ -11,20 +12,11 @@ from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Point
-class DroneInterface:
-    # _instance = None
-    # def __new__(cls, *args, **kwargs):
-    # 	if not PlatformInterface._instance:
-    # 		PlatformInterface._instance = super(PlatformInterface, cls).__new__(cls, *args, **kwargs)
-    # 	return PlatformInterface._instance
-
+class RobotInterface:
     def __init__(self, navdata_name, cmd_topic_name):
-        self.local_network = 1
-        self.k_p_xy = 0.5
-        self.k_p_z = 0.3
-        self.k_d_xy = 0.3
-        self.k_d_z = 0.3
-        self.k_yaw = 0.5
+        self.k_p_xy = 1.0
+        self.k_d_xy = 0.0
+        self.k_yaw = 0.3
         self.limit_cmd_xy = 10
         self.target_point = Twist()
         self.state = Odometry()
@@ -34,11 +26,8 @@ class DroneInterface:
         self.pub_move_cmd = rospy.Publisher(cmd_topic_name, Twist, queue_size=1)
         rospy.on_shutdown(self.Reset)
 
-    # def ReceiveNavdata(self,navdata):
-    # 	# Although there is a lot of data in this packet, we're only interested in the state at the moment
-    # 	self.status = navdata.state
     def Reset(self):
-        print(" shut down the ros...")
+        print("shutting down the ros...")
 
     def callback_pose(self, msg):
         self.state = msg
@@ -70,29 +59,34 @@ class DroneInterface:
     def run(self):
         cmd = Twist()
         if (self.get_target):
-        	err_x = (self.target_point.linear.x - self.state.pose.pose.position.x)
-        	err_y = (self.target_point.linear.y - self.state.pose.pose.position.y)
-        	euler_angle = self.quat2eul(self.state.pose.pose.orientation)
-            cmd.linear.x = self.k_p_xy * (err_x * math.cos(euler_angle) + err_y * math.sin(euler_angle))
-            cmd.linear.y = self.k_p_xy * (- err_x * math.sin(euler_angle) + err_y * math.cos(euler_angle))
-            cmd.linear.z = self.k_p_z * (self.target_point.linear.z - self.state.pose.pose.position.z)
-            
-            cmd.angular.z = self.k_yaw * (self.target_point.angular.z - euler_angle.z)
+            err_x = (self.target_point.linear.x - self.state.pose.pose.position.x)
+            err_y = (self.target_point.linear.y - self.state.pose.pose.position.y)
+            euler_angle = self.quat2eul(self.state.pose.pose.orientation)
+            yaw = euler_angle.z
+            cmd.linear.x = self.k_p_xy * (err_x * math.cos(yaw) + err_y * math.sin(yaw))
+            cmd.linear.y = self.k_p_xy * (- err_x * math.sin(yaw) + err_y * math.cos(yaw))
+            cmd.angular.z = self.k_yaw * (self.target_point.angular.z - yaw)
         self.move_cmd_send(cmd)
         
 
 if __name__ == "__main__":
-    rospy.init_node('DroneInterface1')
-    drone_1 = DroneInterface('/uav1/ground_truth/state', '/A/uav1/velocity_cmd')
-    sub_target1 = rospy.Subscriber('/uav1/target_point', drone_1.set_target_point)
+    rospy.init_node('CarMecanum')
+    car_mecanum = RobotInterface('/A/car2/odom', '/A/car2/cmd_vel')
+    # sub_target1 = rospy.Subscriber('/target_point', Twist, car_mecanum.set_target_point)
+    rate = rospy.Rate(50)
+    t_start = time.time()
     target = Twist()
-    target.linear.x = 5
-    target.linear.y = 4
-    target.linear.z = 2
+    target.linear.x = 0
+    target.linear.y = 0
     target.angular.z = 0
+    w = 0.05
     while not rospy.is_shutdown():
-        drone_1.set_target_point(target)
-        drone_1.run()
+        t = time.time() - t_start;
+        target.linear.x = 5.0 * math.sin(w*2*t)
+        target.linear.y = 10.0 * math.sin(w*t)
+        car_mecanum.set_target_point(target)
+        car_mecanum.run()
+        rate.sleep()
 
 
 
